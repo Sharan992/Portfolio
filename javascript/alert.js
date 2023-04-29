53b4671de81a6e3dc928b1b3dc7c3debbf3bf15a
sharanmanisandela@gmail.com <sharanmanisandela@gmail.com>
2023-04-29 16:05:06 +0530

    First Commit

---
+++ javascript/alert.js
@@ -0,0 +1,10 @@
+function sharan(){
+    var name = 'Phone : 6302935992 Email : sharanmanisandela@gmail.com';
+    window.alert(name);
+    
+}
+
+
+    function showForm() {
+         document.getElementById('formElement').style.display = 'block'
+    } 
